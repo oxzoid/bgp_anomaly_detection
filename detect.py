1c84ac8e2001cf98b639dcc53f43322df6e60c84
oxzoid <you@example.com>
2026-03-30 23:01:15 +0530

    added allow origins to github pages only

--- detect.py
+++ detect.py
@@ -22,7 +22,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://oxzoid.github.io"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
